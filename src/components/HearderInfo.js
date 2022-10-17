43a9f3a2fc328be07d2778102d99189805c66fdf
Arnaud293 <arnaudlt293@gmail.com>
2022-10-17 18:31:00 +0200

    rel header components with percentChange subcomponent

--- src/components/HearderInfo.js
+++ src/components/HearderInfo.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import PercentChange from './PercentChange';
 
 const HearderInfo = () => {
 
@@ -17,7 +18,15 @@ const HearderInfo = () => {
                     <h1><img src='./assets/logo.png'/> Watch Tower</h1>
                 </li>
                 <li>
-                    Crypto-monnaies : {headerData.active_cryptocurrencies}
+                    Crypto-monnaies : {headerData.active_cryptocurrencies && headerData.active_cryptocurrencies.toLocaleString()}
+                </li>
+                <li>
+                    Marchés : {headerData.markets && headerData.markets}
+                </li>
+            </ul>
+            <ul className="info-mkt">
+                <li className="global-market">
+                    Global Market Cap : <PercentChange percent={headerData.market_cap_change_percentage_24h_usd}/>
                 </li>
             </ul>
         </div>
