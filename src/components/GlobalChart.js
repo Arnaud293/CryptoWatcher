670b2068ba04b143360cbc4e1d7187ec6607a381
Arnaud293 <arnaudlt293@gmail.com>
2022-10-18 12:00:10 +0200

    preparing data for threemap

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const GlobalChart = ({coinsData}) => {
+
+    const [dataArray, setDataArray] = useState([]);
+
+    useEffect(() => {
+        let chartData = [];
+
+        if(coinsData.length > 0){
+            for(let i = 0; i < 45; i++){
+                chartData.push({
+                    name: coinsData[i].symbol.toUpperCase() + ' ' + coinsData[i].market_cap_change_percentage_24h.toFixed(1) + "%",
+                    size: coinsData[i].market_cap,
+                    fill: null,
+
+                })
+            }
+        }
+        setDataArray(chartData);
+    }, [coinsData])
+
+
     return (
         <div className='global-chart'>
             <h1>Test</h1>
