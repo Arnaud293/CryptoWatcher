af208fc9def5bca76e81df1267ee1ac556e19c8d
Arnaud293 <arnaudlt293@gmail.com>
2022-10-19 17:10:43 +0200

    First chart render

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import colors from '../styles/_settings.scss';
 
 const CoinChart = ({coinId, coinName}) => {
 
@@ -33,7 +35,7 @@ const CoinChart = ({coinId, coinName}) => {
             }
             setCoinData(dataArray);
         })
-    }, [])
+    }, [duration])
 
     return (
         <div className='coin-chart'>
@@ -47,6 +49,20 @@ const CoinChart = ({coinId, coinName}) => {
                 )
             })}
            </div>
+           <AreaChart width={680} height={250} data={coinData} margin={{top : 10, right: 0, left: 100, bottom : 0}}>
+                <defs>
+                    <linearGradient id='colorUv' x1='0' y1="0" x2="0" y2="1">
+                        <stop offset="7%" stopColor={colors.color1} stopOpacity={0.8} />
+                        <stop offset="93%" stopColor={colors.white1} stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="date" />
+                <YAxis domain={["auto", "auto"]}/>
+                <CartesianGrid strokeDasharray= "3 3"/>
+                <Tooltip />
+                <Area type='monotone' dataKey="price" stroke={colors.color1} fillOpacity={1} fill='url(#colorUv)'/>
+
+           </AreaChart>
         </div>
     );
 };
