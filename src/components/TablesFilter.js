d494b2bf328033254ec1418c68d7b052c4bfacab
Arnaud293 <arnaudlt293@gmail.com>
2022-10-20 14:35:48 +0200

    Show/Hide favlist

--- src/components/TablesFilter.js
+++ src/components/TablesFilter.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setStableState } from '../actions/stable.action';
+import {setListDisplay} from '../actions/list.action';
 
 const TablesFilter = () => {
 
     const [showStable, setShowStable] = useState(true);
+    const [showFavlist, setShowFavlist] = useState(false);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(setStableState(showStable))
-    },[showStable])
+        dispatch(setStableState(showStable));
+        dispatch(setListDisplay(showFavlist));
+    },[showStable, showFavlist])
 
     return (
         <div className='table-filters'>
@@ -20,10 +23,10 @@ const TablesFilter = () => {
                     <label htmlFor="stableCoin">{showStable ? "Avec Stable Coin" : "Sans Stable Coin"}</label>
                 </div>
             
-                <div className="no-list-btn">
+                <div className={showFavlist ? "no-list-btn" : "no-list-btn active"} onClick={() => setShowFavlist(false)}>
                     <p>Aucune liste</p>
                 </div>
-                <div className="fav-list">
+                <div className={showFavlist ? "fav-list active" : "fav-list"} onClick={() => setShowFavlist(true)}>
                     <p>Liste des favoris</p>
                     <img src='./assets/star-full.svg' alt="star-icon"/>
                 </div>
