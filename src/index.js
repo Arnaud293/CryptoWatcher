35e976a52aaa124b18349263b6ec2b55988592be
Arnaud293 <arnaudlt293@gmail.com>
2022-10-20 13:22:36 +0200

    Preparing store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/index.scss';
 import App from './App';
 
+// Redux
+import {  Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-);
+    <Provider store={store}>
+        <App />
+    </Provider>
+    );
