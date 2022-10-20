d0ea5ebeced4fe9e9be273751ec1b0db1c7051bc
Arnaud293 <arnaudlt293@gmail.com>
2022-10-20 13:33:10 +0200

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
 import {combineReducers} from "redux";
+import stableReducer from "./stable.reducer";
 
 
-export default combineReducers({})
+export default combineReducers({
+    stableReducer,
+})
